00624030a9538106e35bd9ab25c7dfdaa801b4c0
aniketkundu7 <aniketkundu76@gmail.com>
2022-05-27 22:13:02 +0530

    Working on Question Level Controller

--- app/Http/Controllers/QuestionLevelController.php
+++ app/Http/Controllers/QuestionLevelController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\QuestionLevel;
 use App\Http\Requests\StoreQuestionLevelRequest;
 use App\Http\Requests\UpdateQuestionLevelRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class QuestionLevelController extends Controller
 {
@@ -13,9 +15,11 @@ class QuestionLevelController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function get_question_level()
     {
-        //
+        $questionlevel=QuestionLevel::get();
+        return $questionlevel;
+        
     }
 
     /**
@@ -23,9 +27,26 @@ class QuestionLevelController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function save_question_level(Request $request)
     {
-        //
+        $rules = array(
+         'levelName' => 'required|max:20'
+        );
+         $messages =  array(
+        'levelName.required' => 'Please enter a level name',
+        'levelName.unique' => 'Please enter a unique level name'
+            
+
+        );
+        $validator =Validator::make($request->all(),$rules,$messages);
+        if($validator->fails()){
+        return response()->json(['success'=>1,'data'=>$validator->messages()], 200,[],JSON_NUMERIC_CHECK);
+        }
+        $questionlevel = new QuestionLevel();
+        $questionlevel->level_name = $request->input('levelName');
+        $questionlevel->update();
+        return response()->json(['success'=>1,'data'=>$questionlevel], 200,[],JSON_NUMERIC_CHECK);
+ 
     }
 
     /**
@@ -68,9 +89,23 @@ class QuestionLevelController extends Controller
      * @param  \App\Models\QuestionLevel  $questionLevel
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateQuestionLevelRequest $request, QuestionLevel $questionLevel)
+    public function update_question_level(Request $request, QuestionLevel $questionLevel)
     {
-        //
+        $rules = array(
+        'levelName' => 'required|max:30'
+        );
+        $messages = array(
+       'levelName.required' => 'Please enter a proper level name',
+        );
+        $validator =Validator::make($request->all(),$rules,$messages);
+        if($validator->fails()){
+        return response()->json(['success'=>1,'data'=>$validator->messages()], 200,[],JSON_NUMERIC_CHECK);
+        }
+        $questionLevel = QuestionLevel::findOrFail ($request->id);
+        $questionLevel -> level_name = $request ->input('questionLevelName');
+        $questionLevel -> update();
+        //return $questionLevel;
+        return response()->json(['success'=>1,'data'=>$questionLevel],200,[],JSON_NUMERIC_CHECK);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\QuestionLevelController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,4 +25,10 @@ Route::group(array('prefix' => 'dev'), function() {
     Route::get("getSubjects", [SubjectController::class,'get_subject']);
     Route::post("saveSubject", [SubjectController::class,'save_subject']);
     Route::patch("updateSubject", [SubjectController::class,'update_subject']);
+
+    Route::get("questionLevel", [QuestionLevelController::class,'get_question_level']);
+    Route::post("questionLevel", [QuestionLevelController::class,'save_question_level']);
+    Route::patch("questionLevel", [QuestionLevelController::class,'update_question_level']);
+
+    
 });
